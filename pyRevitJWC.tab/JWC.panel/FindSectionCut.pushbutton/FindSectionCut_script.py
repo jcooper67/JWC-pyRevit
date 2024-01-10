05fee9be6c076e213fe46455b7d668d5c3b18693
Jcooper67 <cooperwjacob@gmail.com>
2024-01-10 16:46:35 -0500

    Removed Comments

--- pyRevitJWC.tab/JWC.panel/FindSectionCut.pushbutton/FindSectionCut_script.py
+++ pyRevitJWC.tab/JWC.panel/FindSectionCut.pushbutton/FindSectionCut_script.py
@@ -14,36 +14,23 @@ selectedSectionID = uiDoc.Selection.GetElementIds()[0]
 selectedSectionID = doc.GetElement(selectedSectionID).ViewId
 
 
-#print(selectedSectionID)
+
 
 selectedSectionName = doc.GetElement(selectedSectionID).Name
 detailID = doc.GetElement(selectedSectionID).GetTypeId()
-#print(detailID)
-# print(selectedSectionName)
+
 
 collector = FilteredElementCollector(doc)
-#allElements = collector.OfCategory(detailID)
-#allElements = collector.OfClass(View)
 allElements = collector.OfCategory(BuiltInCategory.OST_Viewers)
-#print(allElements)
 
 for element in allElements:
-	#print(element.Name)
-
 	try:
 		if element.Name == selectedSectionName:
-			print("Searching for: ")
-			print(selectedSectionName)
-			print("Match Found: ")
-			print(element.Name)
-			print(element.Id)
-
 			
 			ownerId = element.OwnerViewId
 			ownerView = doc.GetElement(ownerId)
 			ownerViewName = ownerView.Name
-			print(ownerViewName)
-			
+			print(ownerViewName)	
 			
 	except AttributeError:
 	
